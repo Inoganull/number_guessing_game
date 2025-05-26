a3e18a289eafa42ea8cf8f479a52927be78a8206
Kyaw Min Hein <kyawminhein101@gmail.com>
2025-05-26 12:53:18 +0000

    fix: handle non-integer input validation

--- number_guess.sh
+++ number_guess.sh
@@ -17,3 +17,30 @@ else
   IFS="|" read DB_USERNAME GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
   echo "Welcome back, $DB_UERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+# Generate random number
+SERCRET=$(( RANDOM % 1000 + 1 ))
+TRIES=0
+
+# Strat guessing
+echo "Guess the secret number between 1 and 1000:"
+while true; do
+  read GUESS
+  ((TRIES++))
+
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  if (( GUESS == SECRET )); then
+    echo "You guessed it in $TRIES tries. The secret number was $SECRET. Nice job!"
+    break
+  elif (( GUESS < SECRET )); then
+    echo "It's higher than that, guess again:"
+  else
+    echo "It's lower than that, guess again:"
+  fi
+
+done
+
